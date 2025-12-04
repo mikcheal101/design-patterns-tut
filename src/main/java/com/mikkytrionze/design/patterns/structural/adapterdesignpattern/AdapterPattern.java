bcf8a115e03468ccdd0dae3b0bc8d7395a6f6832
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-04 11:17:51 +0100

    init adapter pattern

---
+++ src/main/java/com/mikkytrionze/design/patterns/structural/adapterdesignpattern/AdapterPattern.java
@@ -0,0 +1,13 @@
+package com.mikkytrionze.design.patterns.structural.adapterdesignpattern;
+
+public class AdapterPattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Structural Design Patterns:");
+        System.out.println("Adapter Design Pattern:");
+
+        // Implementation of Adapter Pattern would go here
+    }
+}
