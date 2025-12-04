c0222cfcaca070219dd0893061c739c44a95b575
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-04 11:31:51 +0100

    created use case in adapter pattern

--- src/main/java/com/mikkytrionze/design/patterns/structural/adapterdesignpattern/AdapterPattern.java
+++ src/main/java/com/mikkytrionze/design/patterns/structural/adapterdesignpattern/AdapterPattern.java
@@ -9,5 +9,22 @@ public class AdapterPattern {
         System.out.println("Adapter Design Pattern:");
 
         // Implementation of Adapter Pattern would go here
+        // The old client pattern / style.
+        IMultiRestoApp multiRestoApp = new MultiRestoApp();
+        multiRestoApp.displayMenu(new XmlData() {
+            // Implementation details
+        });
+        multiRestoApp.displayRecommendations(new XmlData() {
+            // Implementation details
+        });
+
+        // The new fancy UI service with an adapter.
+        IMultiRestoApp fancyUIServiceAdapter = new FancyUIServiceAdapter();
+        fancyUIServiceAdapter.displayMenu(new XmlData() {
+            // Implementation details
+        });
+        fancyUIServiceAdapter.displayRecommendations(new XmlData() {
+            // Implementation details
+        });
     }
 }
