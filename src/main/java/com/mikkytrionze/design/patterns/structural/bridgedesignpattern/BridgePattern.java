60f43f7ba9de41e8def3fbf51e6f4ad85a4bc498
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-04 11:38:06 +0100

    init bridge pattern

---
+++ src/main/java/com/mikkytrionze/design/patterns/structural/bridgedesignpattern/BridgePattern.java
@@ -0,0 +1,13 @@
+package com.mikkytrionze.design.patterns.structural.bridgedesignpattern;
+
+public class BridgePattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Structural Design Patterns:");
+        System.out.println("Bridge Design Pattern:");
+        
+        // Implementation of Bridge Pattern would go here
+    }
+}
