1a8045ed819247ce8282b222faa835cdd46fdd5e
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-04 11:32:44 +0100

    tested use case in main

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.creational.prototypedesignpattern.PrototypePattern;
+import com.mikkytrionze.design.patterns.structural.adapterdesignpattern.AdapterPattern;
 
 public class Main {
     public static void main(String[] args) {
-        var prototypedesignpattern = new PrototypePattern();
-        prototypedesignpattern.execute();
+        var adapterdesignpattern = new AdapterPattern();
+        adapterdesignpattern.execute();
     }
 }
