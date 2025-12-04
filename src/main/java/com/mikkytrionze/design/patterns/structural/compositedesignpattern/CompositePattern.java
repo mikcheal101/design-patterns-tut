5d2f5c37b8538daa9b63ddc56b50ea944ebc967e
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-04 13:30:20 +0100

    init composite design pattern

---
+++ src/main/java/com/mikkytrionze/design/patterns/structural/compositedesignpattern/CompositePattern.java
@@ -0,0 +1,14 @@
+package com.mikkytrionze.design.patterns.structural.compositedesignpattern;
+
+public class CompositePattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Structural Design Patterns:");
+        System.out.println("Composite Design Pattern:");
+
+        // Implementation of Composite Pattern would go here
+    }
+
+}
