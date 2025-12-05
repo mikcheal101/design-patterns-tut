1aa0023281edd519436ac93378767e34356fadb3
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-05 15:33:25 +0100

    corrected user id from string to uuid

--- src/main/java/com/mikkytrionze/design/patterns/structural/facadedesignpattern/UIService.java
+++ src/main/java/com/mikkytrionze/design/patterns/structural/facadedesignpattern/UIService.java
@@ -1,10 +1,12 @@
 package com.mikkytrionze.design.patterns.structural.facadedesignpattern;
 
+import java.util.UUID;
+
 public class UIService {
 
-    public static String getLoggedInUserId() {
+    public static UUID getLoggedInUserId() {
         // Simulate fetching the logged-in user ID
-        return "user123";
+        return UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
     }
 
     public boolean logIn(String username, String password) {
