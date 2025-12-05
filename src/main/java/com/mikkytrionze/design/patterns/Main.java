8aa9b23026b27b1f159a9df18a5bb1533bf483ee
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-05 15:47:02 +0100

    proxy pattern init

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.structural.facadedesignpattern.FacadePattern;
+import com.mikkytrionze.design.patterns.structural.proxydesignpattern.ProxyPattern;
 
 public class Main {
     public static void main(String[] args) {
-        var facadedesignpattern = new FacadePattern();
-        facadedesignpattern.execute();
+        var proxydesignpattern = new ProxyPattern();
+        proxydesignpattern.execute();
     }
 }
---
+++ src/main/java/com/mikkytrionze/design/patterns/structural/proxydesignpattern/ProxyPattern.java
@@ -0,0 +1,13 @@
+package com.mikkytrionze.design.patterns.structural.proxydesignpattern;
+
+public class ProxyPattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Structural Design Patterns:");
+        System.out.println("Proxy Design Pattern:");
+
+        // Implementation of Proxy Pattern would go here
+    }
+}
