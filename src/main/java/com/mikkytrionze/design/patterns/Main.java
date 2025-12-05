559504852e159b94b97d2e0c23f340b7e93d80c0
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-05 21:48:57 +0100

    Flyweight init

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.structural.proxydesignpattern.ProxyPattern;
+import com.mikkytrionze.design.patterns.structural.flyweightdesignpattern.FlyweightPattern;
 
 public class Main {
     public static void main(String[] args) {
-        var proxydesignpattern = new ProxyPattern();
-        proxydesignpattern.execute();
+        var flyweightdesignpattern = new FlyweightPattern();
+        flyweightdesignpattern.execute();
     }
 }
---
+++ src/main/java/com/mikkytrionze/design/patterns/structural/flyweightdesignpattern/FlyweightPattern.java
@@ -0,0 +1,14 @@
+package com.mikkytrionze.design.patterns.structural.flyweightdesignpattern;
+
+public class FlyweightPattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Structural Design Patterns:");
+        System.out.println("Flyweight Design Pattern");
+
+        // Implementation of Flyweight Design Pattern would go here
+    }
+
+}
