aa07f124574b8554fbbb8edb8e31ae6bb35ac718
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-05 15:36:19 +0100

    implemented init facade concern

--- src/main/java/com/mikkytrionze/design/patterns/structural/facadedesignpattern/FacadePattern.java
+++ src/main/java/com/mikkytrionze/design/patterns/structural/facadedesignpattern/FacadePattern.java
@@ -9,6 +9,15 @@ public class FacadePattern {
         System.out.println("Facade Design Pattern:");
 
         // Implementation of Facade Pattern would go here
+        DatabaseService databaseService = new DatabaseService();
+        User user = databaseService.getUser(UIService.getLoggedInUserId());
+        System.out.println("User retrieved: " + user.getName());
+        
+        CryptoService bitcoinService = CryptoFactory.getCryptoService("Bitcoin");
+        bitcoinService.buyCurrency(user, 1000.0);
+
+        CryptoService ethereumService = CryptoFactory.getCryptoService("Ethereum");
+        ethereumService.buyCurrency(user, 500.0);
     }
 
 }
