b81c89db64196c0d1a254d866c0da53cd5b5c82e
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-06 16:22:22 +0100

    init chain of responsibility pattern

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.structural.flyweightdesignpattern.FlyweightPattern;
+import com.mikkytrionze.design.patterns.behavioural.ChainOfResponsibilityPattern;
 
 public class Main {
     public static void main(String[] args) {
-        var flyweightdesignpattern = new FlyweightPattern();
-        flyweightdesignpattern.execute();
+        var chainofresponsibility = new ChainOfResponsibilityPattern();
+        chainofresponsibility.esecute();
     }
 }
---
+++ src/main/java/com/mikkytrionze/design/patterns/behavioural/ChainOfResponsibilityPattern.java
@@ -0,0 +1,13 @@
+package com.mikkytrionze.design.patterns.behavioural;
+
+public class ChainOfResponsibilityPattern {
+
+    public void esecute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Behavioural Design Patterns:");
+        System.out.println("Chain of Responsibility Pattern");
+
+        // Implementation of Chain of Responsibility Pattern would go here.
+    }
+}
