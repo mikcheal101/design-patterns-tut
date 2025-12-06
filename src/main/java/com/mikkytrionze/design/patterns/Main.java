431415b7e59db3ec294a342a2748f8c0129fa2c8
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-06 18:09:20 +0100

    init strategy pattern

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.behavioural.chainofresponsibilitypattern.ChainOfResponsibilityPattern;
+import com.mikkytrionze.design.patterns.behavioural.strategydesignpattern.StrategyPattern;
 
 public class Main {
     public static void main(String[] args) {
-        var chainofresponsibility = new ChainOfResponsibilityPattern();
-        chainofresponsibility.execute();
+        var strategydesignpattern = new StrategyPattern();
+        strategydesignpattern.execute();
     }
 }
---
+++ src/main/java/com/mikkytrionze/design/patterns/behavioural/strategydesignpattern/StrategyPattern.java
@@ -0,0 +1,13 @@
+package com.mikkytrionze.design.patterns.behavioural.strategydesignpattern;
+
+public class StrategyPattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Behavioural Design Patterns:");
+        System.out.println("Strategy Pattern");
+
+        // Implementation goes here
+    }
+}
