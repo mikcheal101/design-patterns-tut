ae02166d3bdab222b9c33aeadf20319c8b6f8dec
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-07 23:14:25 +0100

    inite template pattern

--- src/main/java/com/mikkytrionze/design/patterns/Main.java
+++ src/main/java/com/mikkytrionze/design/patterns/Main.java
@@ -1,10 +1,10 @@
 package com.mikkytrionze.design.patterns;
 
-import com.mikkytrionze.design.patterns.behavioural.observerdesignpattern.ObserverPattern;
+import com.mikkytrionze.design.patterns.behavioural.templatedesignpattern.TemplatePattern;
 
 public class Main {
     public static void main(String[] args) {
-        var observerdesignpattern = new ObserverPattern();
-        observerdesignpattern.execute();
+        var templatedesignpattern = new TemplatePattern();
+        templatedesignpattern.execute();
     }
 }
---
+++ src/main/java/com/mikkytrionze/design/patterns/behavioural/templatedesignpattern/TemplatePattern.java
@@ -0,0 +1,14 @@
+package com.mikkytrionze.design.patterns.behavioural.templatedesignpattern;
+
+public class TemplatePattern {
+
+    public void execute() {
+        System.out.println("Design Patterns in Java");
+        System.out.println("-----------------------");
+        System.out.println("Behavioural Design Patterns:");
+        System.out.println("Template Design Pattern");
+
+        /// Implementation goes here
+        
+    }
+}
